3b99c24bb846d335a567b6fe554ace7ffe7a6964
Retnoayu7776 <sayaretno844@gmail.com>
2025-09-10 15:11:59 +0700

    belajar pop up

---
+++ Belajartn.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    alert('selamat datang di webiste rtnayu');
+};
+confirm("Apakah km tau apa yg  lebih lutcuw dari kamu?");
+prompt("YA G ADA LAA CUMA KAMU YANG PALING LUTCUW:");
+Date(10)
+Math.random()
+Math.round(4.6)
+parseInt("234")
+setTimeout(function() { alert("selamat datang di website rtnayu");}, 3000);
